9c389b8a69eccd664be191e507d7cf17e8319b6f
Joshix <joshix.dev@mailbox.org>
2026-04-29 18:00:00 +0000

    evdev

--- input_handling.py
+++ input_handling.py
@@ -1,11 +1,6 @@
 import os
-
-os.environ["SDL_VIDEODRIVER"] = "dummy"
-os.environ["SDL_JOYSTICK_ALLOW_BACKGROUND_EVENTS"] = "1"
-
 import time
 import threading
-import pygame
 
 dx = 0
 dy = 0
@@ -38,46 +33,25 @@ def has_any_input() -> bool:
 def input_handling():
     global dx, dy
 
-    pygame.init()
+    import evdev
 
-    while pygame.joystick.get_count() == 0:
-        time.sleep(1)
-
-    joystick = pygame.joystick.Joystick(0)
+    device = evdev.InputDevice('/dev/input/event1')
 
     print("Starting listening to joysticks")
 
-    while True:
-        for j in range(pygame.joystick.get_count()):
-            joy = pygame.joystick.Joystick(j)
-            print("axis", j, [joy.get_axis(axis) for axis in range(joy.get_numaxes())])
-            print("btns", j, [joy.get_button(button) for button in range(joy.get_numbuttons())])
-
-        for axis in range(min(2, joystick.get_numaxes())):
-            value = joystick.get_axis(axis)
-            # print("axis", axis, "has value", value)
-            if abs(value) > 0.5:
-                normed_value = 1 if value > 0 else -1
-                if value == 0:
-                    dx = normed_value
-                else:
-                    dy = normed_value
-
-        for button in BUTTONS:
-            if not (value := joystick.get_button(button)) and LAST_BUTTON_PRESSED.get(button, False):
-                print("released", value)
-                BUTTON_PRESSES[button] = True
-            elif value:
-                print("pressed", value)
-            LAST_BUTTON_PRESSED[button] = value
+    for event in device.read_loop():
+        if event.type == evdev.ecodes.EV_KEY:
+            print(f"key {event.code=} {event.value=}")
+        elif event.type == evdev.ecodes.EV_ABS:
+            print(f"abs {event.code=} {event.value=}")
+        else:
+            print(event)
 
         time.sleep(0.1)
 
 
 def vibrate_controller():
-    for j in range(pygame.joystick.get_count()):
-        joystick = pygame.joystick.Joystick(j)
-        joystick.rumble(100, 400, 1)
+    pass
 
 
 def start() -> threading.Thread:
